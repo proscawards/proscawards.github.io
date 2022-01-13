9b39413aaa365b593108727c262ffe84f7692b22
SC ONG <proscawards99@gmail.com>
2022-01-13 20:52:04 +0800

    Version 1.0.6
    UPDATES:
    - Improved clarity on light theme and dark theme.
    - Minor tweaks on styling.
    - Updated Experience section.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -17,29 +17,31 @@ export class HeaderComponent extends TypeWriterComponent{
   lightThemeBtnOnClick(){
     $("#darkThemeBtn").show();
     $("#lightThemeBtn").hide();
-    $(document.documentElement).css("--currentBgCodeColor", "#f1f3f4");
+    $(document.documentElement).css("--currentBgCodeColor", "#F7F7F7");
     $(document.documentElement).css("--currentFontCodeColor", "#5888AD");
-    $(document.documentElement).css("--oppositeBgCodeColor", "#283142");
-    $(document.documentElement).css("--oppositeFontCodeColor", "#0d904f");
+    $(document.documentElement).css("--oppositeBgCodeColor", "#242C3C");
+    $(document.documentElement).css("--oppositeFontCodeColor", "#5888AD");
     $(document.documentElement).css("--currentFontDescColor", "#3b78e7");
-    $(document.documentElement).css("--currentBgSdColor", "rgb(40,49,66,1)");
-    $(document.documentElement).css("--currentBgSdGradColor", "rgb(40,49,66,1)");
-    $(document.documentElement).css("--currentUEFilter", "invert(100%)");
-    $(document.documentElement).css("--oppositeUEFilter", "invert(0%)");
+    $(document.documentElement).css("--currentBgSdColor", "rgb(255,255,255,.8)");
+    $(document.documentElement).css("--currentBgSdGradColor", "rgb(255,255,255,.8)");
+    $(document.documentElement).css("--currentUEFilter", "invert(0%)");
+    $(document.documentElement).css("--oppositeUEFilter", "invert(100%)");
+    $(document.documentElement).css("--currentBgCommentColor", "rgba(255,255,255,1)");
   }
   
   darkThemeBtnOnClick(){
     $("#lightThemeBtn").show();
     $("#darkThemeBtn").hide();
-    $(document.documentElement).css("--currentBgCodeColor", "#283142");
-    $(document.documentElement).css("--currentFontCodeColor", "#0d904f");
-    $(document.documentElement).css("--oppositeBgCodeColor", "#f1f3f4");
+    $(document.documentElement).css("--currentBgCodeColor", "#242C3C");
+    $(document.documentElement).css("--currentFontCodeColor", "#5888AD");
+    $(document.documentElement).css("--oppositeBgCodeColor", "#F7F7F7");
     $(document.documentElement).css("--oppositeFontCodeColor", "#5888AD");
     $(document.documentElement).css("--currentFontDescColor", "#4dd0e1");
-    $(document.documentElement).css("--currentBgSdColor", "rgb(0,0,0,.3)");
+    $(document.documentElement).css("--currentBgSdColor", "rgb(40,49,66,.8)");
     $(document.documentElement).css("--currentBgSdGradColor", "linear-gradient(90deg, rgb(0,0,0,.3) 0%, rgb(0,0,0,.3) 40%, rgba(0,0,0,0) 100%)");  
-    $(document.documentElement).css("--currentUEFilter", "invert(0%)");
-    $(document.documentElement).css("--oppositeUEFilter", "invert(100%)");
+    $(document.documentElement).css("--currentUEFilter", "invert(100%)");
+    $(document.documentElement).css("--oppositeUEFilter", "invert(0%)");
+    $(document.documentElement).css("--currentBgCommentColor", "rgba(59,72,97,1)");
   }
 
   //Click event to scroll to top

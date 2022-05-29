0f5196f8d60ce61af1f6a25238c28722fea037e3
proscawards <proscawards99@gmail.com>
2022-05-29 15:17:54 +0800

    Version 1.4.1.2
    UPDATES:
    - Hotfix on single month issue.
    - Updated theme color in manifest.

--- src/app/utils/Duration.ts
+++ src/app/utils/Duration.ts
@@ -2,9 +2,13 @@ import { differenceInCalendarMonths, parse } from 'date-fns';
 
 //Calculate the duration of experience
 export default function Duration(dateArr: string){
-    let arr: string[] = dateArr.split(" - ");
-    let start: any = parse(arr[0], 'LLLL yyyy', new Date());
-    let end: any = new Date();
+  let arr: string[] = dateArr.split(" - ");
+  let start: any = parse(arr[0], 'LLLL yyyy', new Date());
+  let end: any = new Date();
+  if (arr.length === 1) {
+    return "1 month";
+  }
+  else {
     if (arr[1] != "PRESENT"){
       end = parse(arr[1], 'LLLL yyyy', new Date());
     }
@@ -14,5 +18,6 @@ export default function Duration(dateArr: string){
     if (month >= 6 && diff <= 12) {
       year = 0;
     } 
-    return `${year != 0 ? year : ''} ${year == 1 ? 'year' : year == 0 ? '' : 'years'} ${month != 0 ? month : ''} ${month == 1 ? 'month' : month != 0 ? 'months' : ''}`
+    return `${year != 0 ? year : ''} ${year == 1 ? 'year' : year == 0 ? '' : 'years'} ${month != 0 ? month : ''} ${month == 1 ? 'month' : month != 0 ? 'months' : ''}`;
   }
+}
